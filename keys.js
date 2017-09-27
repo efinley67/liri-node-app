ad410a57deec78358dcc0ae23dac8dc770c2d847
unknown <efinley67@gmail.com>
2017-09-26 18:29:06 -0600

    updated hw files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+  consumer_key: "UoxucMZciTCvynWBWjVfA5qf9",
+  consumer_secret: "IKp6jXWkptrRwOcduSY5CYBcq9uWbUQbw8yVLc5DdNh1Qedde3",
+  access_token_key: "911681239546978304-rHaoY7jc487tcpx8HIpcFwCBzdo9ss4",
+  access_token_secret: "1jyyvuvNGSU2GCREyDNGg5bhWsHQT8uFFNp7ZcXkljvpU",
+}
+
+module.exports = twitterKeys;
