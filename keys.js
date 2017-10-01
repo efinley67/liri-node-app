a66db4631577f71ee0b993559fa426bcd798908c
unknown <efinley67@gmail.com>
2017-10-01 14:56:13 -0600

    updated HW files

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log("this is loaded");
+// console.log("this is loaded");
 
 var twitterKeys = {
   consumer_key: "UoxucMZciTCvynWBWjVfA5qf9",
@@ -6,5 +6,7 @@ var twitterKeys = {
   access_token_key: "911681239546978304-rHaoY7jc487tcpx8HIpcFwCBzdo9ss4",
   access_token_secret: "1jyyvuvNGSU2GCREyDNGg5bhWsHQT8uFFNp7ZcXkljvpU",
 }
+
+// console.log(twitterKeys);
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,8 +1,21 @@
 var request = require("request");
+var keys = require("./keys");
+var twitterKeys = keys;
 
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+var consumer_key = twitterKeys.consumer_key;
+var consumer_secret = twitterKeys.consumer_secret;
+var access_token_key = twitterKeys.access_token_key;
+var access_token_secret = twitterKeys.access_token_secret;
 
-  if (!error && response.statusCode === 200) {
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
+console.log(consumer_key);
+console.log(consumer_secret);
+console.log(access_token_key);
+console.log(access_token_secret);
+
+
+// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+//
+//   if (!error && response.statusCode === 200) {
+//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+//   }
+// });
